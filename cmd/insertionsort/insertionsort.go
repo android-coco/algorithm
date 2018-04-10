d39ef6b2480e59292a2f150f6630ca75ef88bf9f
android-coco <yh_android@163.com>
2018-04-10 20:53:39 +0800

    swich type 优化

--- cmd/insertionsort/insertionsort.go
+++ cmd/insertionsort/insertionsort.go
@@ -28,64 +28,64 @@ func insertionSort(sortable util.Sortable) {
 
 //插入排序升级版
 func insertionSort2(sortable util.Sortable) {
-	switch sortable.(type) {
+	switch value := sortable.(type) {
 	case util.IntArray:
-		intArray, ok := sortable.(util.IntArray)
-		util.CheckConvert(ok)
+		//intArray, ok := sortable.(util.IntArray)
+		//util.CheckConvert(ok)
 		len := sortable.Len()
 		for i := 1; i < len; i++ {
 			//寻找元素arr[i]合适的插入位置
-			e := intArray[i]
+			e := value[i]
 			var j = 0 //保存元素e应该插入的位置
-			for j = i; j > 0 && intArray[j-1] > e; j-- {
+			for j = i; j > 0 && value[j-1] > e; j-- {
 				//比前面的小  向后挪一位
-				intArray[j] = intArray[j-1]
+				value[j] = value[j-1]
 			}
-			intArray[j] = e //到了 赋值
+			value[j] = e //到了 赋值
 		}
 	case util.Float64Array:
-		float64Array, ok := sortable.(util.Float64Array)
-		util.CheckConvert(ok)
+		//float64Array, ok := sortable.(util.Float64Array)
+		//util.CheckConvert(ok)
 		len := sortable.Len()
 		for i := 1; i < len; i++ {
 			//寻找元素arr[i]合适的插入位置
-			e := float64Array[i]
+			e := value[i]
 			var j = 0 //保存元素e应该插入的位置
-			for j = i; j > 0 && float64Array[j-1] > e; j-- {
+			for j = i; j > 0 && value[j-1] > e; j-- {
 				//比前面的小  向后挪一位
-				float64Array[j] = float64Array[j-1]
+				value[j] = value[j-1]
 			}
-			float64Array[j] = e //到了 赋值
+			value[j] = e //到了 赋值
 		}
 	case util.StringArray:
-		stringArray, ok := sortable.(util.StringArray)
-		util.CheckConvert(ok)
+		//stringArray, ok := sortable.(util.StringArray)
+		//util.CheckConvert(ok)
 		len := sortable.Len()
 		for i := 1; i < len; i++ {
 			//寻找元素arr[i]合适的插入位置
-			e := stringArray[i]
+			e := value[i]
 			var j = 0 //保存元素e应该插入的位置
-			for j = i; j > 0 && stringArray[j-1] > e; j-- {
+			for j = i; j > 0 && value[j-1] > e; j-- {
 				//比前面的小  向后挪一位
-				stringArray[j] = stringArray[j-1]
+				value[j] = value[j-1]
 			}
-			stringArray[j] = e //到了 赋值
+			value[j] = e //到了 赋值
 		}
 	case util.StuArray:
-		stuArray, ok := sortable.(util.StuArray)
-		util.CheckConvert(ok)
+		//stuArray, ok := sortable.(util.StuArray)
+		//util.CheckConvert(ok)
 		len := sortable.Len()
 		for i := 1; i < len; i++ {
 			//寻找元素arr[i]合适的插入位置
-			e := stuArray[i]
+			e := value[i]
 			var j = 0 //保存元素e应该插入的位置
-			for j = i; j > 0 && stuArray[j-1].Score > e.Score; j-- {
+			for j = i; j > 0 && value[j-1].Score > e.Score; j-- {
 				//比前面的小  向后挪一位
-				stuArray[j] = stuArray[j-1]
+				value[j] = value[j-1]
 			}
-			stuArray[j] = e //到了 赋值
+			value[j] = e //到了 赋值
 		}
 	default:
-		fmt.Println("未知类型")
+		fmt.Println("未知类型",value)
 	}
 }
